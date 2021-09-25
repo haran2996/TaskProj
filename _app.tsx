2a9d5bb937a0cc804491f4ff226a8a03e70c1389
haran2996 <91301971+haran2996@users.noreply.github.com>
2021-09-25 09:03:36 +0530

    Delete _app.tsx

--- _app.tsx
+++
@@ -1,7 +0,0 @@
-import { wrapper } from '../store';
-import React from 'react';
-function App({ Component , pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default wrapper.withRedux(App)
